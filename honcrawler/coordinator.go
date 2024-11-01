f6c59007fee131eb3355b52c1290acff555bee85
tao.zhang <tao.zhang@anker.io>
2024-11-01 11:07:56 +0800

    添加注释

--- honcrawler/coordinator.go
+++ honcrawler/coordinator.go
@@ -10,133 +10,134 @@ import (
 )
 
 /*
-	Some constant for the spider
+Some constant for the spider
 */
-const Host = `https://www.wnacg.com`
-const GallaryUrl string = Host + `/albums-index-page-%d.html`
-const UserAgent string = `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0`
-const ImgsPerPage int = 12
+const Host = `https://www.wnacg.com`                                                                        // 定义常量Host，表示目标网站的主机地址
+const GallaryUrl string = Host + `/albums-index-page-%d.html`                                               // 定义常量GallaryUrl，表示相册页面的URL模板
+const UserAgent string = `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0` // 定义常量UserAgent，表示HTTP请求的用户代理
+const ImgsPerPage int = 12                                                                                  // 定义常量ImgsPerPage，表示每页的图片数量
 
 /*
 	The coordinator manager all the concurrent behaviors.
 */
 
 type coordinator struct {
-	tagChannel   chan string     // channel for tag map
-	honChannel   chan *HonDetail // channel for hon data
-	limitChannel chan struct{}
+	tagChannel   chan string     // 定义一个字符串通道，用于传递标签
+	honChannel   chan *HonDetail // 定义一个指向HonDetail的通道，用于传递HonDetail数据
+	limitChannel chan struct{}   // 定义一个空结构体通道，用于限制并发数量
 
-	gWaitGroup sync.WaitGroup
-	dWaitGroup sync.WaitGroup
+	gWaitGroup sync.WaitGroup // 定义一个WaitGroup，用于等待生成任务完成
+	dWaitGroup sync.WaitGroup // 定义一个WaitGroup，用于等待消费任务完成
 
-	tagSet set
-	mutex  sync.Mutex
+	tagSet set        // 定义一个set类型，用于存储标签
+	mutex  sync.Mutex // 定义一个互斥锁，用于保护tagSet的并发访问
 }
 
 var Coordinator = coordinator{
-	honChannel:   make(chan *HonDetail, settings.CrawlerSetting.HonBuffer),
-	tagChannel:   make(chan string, settings.CrawlerSetting.TagBuffer),
-	limitChannel: make(chan struct{}, settings.CrawlerSetting.HonConsumerCount/2),
+	honChannel:   make(chan *HonDetail, settings.CrawlerSetting.HonBuffer),        // 初始化honChannel通道，缓冲区大小为HonBuffer
+	tagChannel:   make(chan string, settings.CrawlerSetting.TagBuffer),            // 初始化tagChannel通道，缓冲区大小为TagBuffer
+	limitChannel: make(chan struct{}, settings.CrawlerSetting.HonConsumerCount/2), // 初始化limitChannel通道，缓冲区大小为HonConsumerCount的一半
 
-	gWaitGroup: sync.WaitGroup{},
-	dWaitGroup: sync.WaitGroup{},
+	gWaitGroup: sync.WaitGroup{}, // 初始化gWaitGroup
+	dWaitGroup: sync.WaitGroup{}, // 初始化dWaitGroup
 
-	tagSet: make(map[string]struct{}),
+	tagSet: make(map[string]struct{}), // 初始化tagSet为一个空的map
 }
 
 // base collector
 var collector = colly.NewCollector(
-	colly.UserAgent(UserAgent),
-	colly.AllowURLRevisit(),
+	colly.UserAgent(UserAgent), // 设置UserAgent
+	colly.AllowURLRevisit(),    // 允许URL重新访问
 )
 
 func init() {
-	collector.SetRequestTimeout(120 * time.Second)
+	collector.SetRequestTimeout(120 * time.Second) // 设置请求超时时间为120秒
 	// Error Handler
 	collector.OnError(func(r *colly.Response, err error) {
-		fmt.Printf("Error %s: %v\n", r.Request.URL, err)
+		fmt.Printf("Error %s: %v\n", r.Request.URL, err) // 定义错误处理函数，打印错误信息
 	})
 	// proxy setting
 	if settings.CrawlerSetting.EnableProxy {
-		collector.SetProxy(settings.CrawlerSetting.ProxyHost)
+		collector.SetProxy(settings.CrawlerSetting.ProxyHost) // 如果启用了代理，设置代理地址
 	}
 }
 
 func (c *coordinator) sendHon(hd *HonDetail) {
-	c.honChannel <- hd
+	c.honChannel <- hd // 将HonDetail数据发送到honChannel通道
 }
 
 func (c *coordinator) sendTag(tag string) {
-	c.tagChannel <- tag
+	c.tagChannel <- tag // 将标签发送到tagChannel通道
 }
 
 func (c *coordinator) generateHon(pSt int, pEnd int) {
-	for i := pSt; i <= pEnd; i++ {
-		c.limitChannel <- struct{}{}
-		c.gWaitGroup.Add(1)
-		go func(i int) {
-			infos := GenGallaryInfos(i)
-			for _, info := range infos {
-				d := GenHonDetails(info)
-				if d.PageNum > 500 {
+	for i := pSt; i <= pEnd; i++ { // 遍历从pSt到pEnd的所有页码
+		c.limitChannel <- struct{}{} // 向limitChannel通道发送一个空结构体，限制并发数量
+		c.gWaitGroup.Add(1)          // 增加gWaitGroup的计数
+		go func(i int) {             // 启动一个新的goroutine
+			infos := GenGallaryInfos(i)  // 生成相册信息
+			for _, info := range infos { // 遍历相册信息
+				d := GenHonDetails(info) // 生成HonDetail数据
+				if d.PageNum > 500 {     // 如果页面数量大于500，跳过
 					continue
 				}
-				if !parseTages(d.Tags) {
+				if !parseTages(d.Tags) { // 如果标签解析失败，跳过
 					continue
 				}
-				if settings.CrawlerSetting.TagConsumerCount > 0 {
-					for _, t := range d.Tags {
-						c.sendTag(t)
+				if settings.CrawlerSetting.TagConsumerCount > 0 { // 如果启用了标签消费者
+					for _, t := range d.Tags { // 遍历标签
+						c.sendTag(t) // 发送标签
 					}
 				}
-				c.sendHon(d)
-				<-c.limitChannel
+				c.sendHon(d)     // 发送HonDetail数据
+				<-c.limitChannel // 从limitChannel通道接收一个空结构体，释放并发限制
 			}
-			c.gWaitGroup.Done()
+			c.gWaitGroup.Done() // 减少gWaitGroup的计数
 		}(i)
 	}
 }
 
 func (c *coordinator) consumeHon(cnt int) {
-	for i := 0; i < cnt; i++ {
-		c.dWaitGroup.Add(1)
-		go func() {
-			for hon := range c.honChannel {
-				Download(hon)
+	for i := 0; i < cnt; i++ { // 启动cnt个goroutine
+		c.dWaitGroup.Add(1) // 增加dWaitGroup的计数
+		go func() {         // 启动一个新的goroutine
+			for hon := range c.honChannel { // 从honChannel通道接收HonDetail数据
+				Download(hon) // 下载HonDetail数据
 			}
-			c.dWaitGroup.Done()
+			c.dWaitGroup.Done() // 减少dWaitGroup的计数
 		}()
 	}
 }
 
 func (c *coordinator) consumeTag(cnt int) {
-	for i := 0; i < cnt; i++ {
-		c.dWaitGroup.Add(1)
-		go func() {
-			for tag := range c.tagChannel {
-				c.mutex.Lock()
-				if !c.tagSet.has(tag) {
-					c.tagSet.insert(tag)
-					SaveTag(tag)
+	for i := 0; i < cnt; i++ { // 启动cnt个goroutine
+		c.dWaitGroup.Add(1) // 增加dWaitGroup的计数
+		go func() {         // 启动一个新的goroutine
+			for tag := range c.tagChannel { // 从tagChannel通道接收标签
+				c.mutex.Lock()          // 加锁
+				if !c.tagSet.has(tag) { // 如果tagSet中没有该标签
+					c.tagSet.insert(tag) // 插入标签到tagSet
+					SaveTag(tag)         // 保存标签
 				}
-				c.mutex.Unlock()
+				c.mutex.Unlock() // 解锁
 			}
+			c.dWaitGroup.Done() // 减少dWaitGroup的计数
 		}()
 	}
 }
 
 func (c *coordinator) Start() {
-	c.consumeHon(settings.CrawlerSetting.HonConsumerCount)
-	if settings.CrawlerSetting.TagConsumerCount > 0 {
-		c.consumeTag(settings.CrawlerSetting.TagConsumerCount)
+	c.consumeHon(settings.CrawlerSetting.HonConsumerCount) // 启动HonDetail消费者
+	if settings.CrawlerSetting.TagConsumerCount > 0 {      // 如果启用了标签消费者
+		c.consumeTag(settings.CrawlerSetting.TagConsumerCount) // 启动标签消费者
 	}
 	c.generateHon(
 		settings.CrawlerSetting.PageStart,
 		settings.CrawlerSetting.PageEnd,
-	)
+	) // 生成HonDetail数据
 
-	c.gWaitGroup.Wait()
-	close(c.honChannel)
-	close(c.tagChannel)
-	c.dWaitGroup.Wait()
+	c.gWaitGroup.Wait() // 等待所有生成任务完成
+	close(c.honChannel) // 关闭honChannel通道
+	close(c.tagChannel) // 关闭tagChannel通道
+	c.dWaitGroup.Wait() // 等待所有消费任务完成
 }
--- honcrawler/gendata.go
+++ honcrawler/gendata.go
@@ -12,92 +12,112 @@ import (
 /*
 	This `go` file will generate the details of ero-hons and collect
 	there tags.
+	这个Go文件将生成ero-hons的详细信息并收集它们的标签。
 */
 
 // from https://www.wnacg.com/albums-index-page-1.html
 // to https://www.wnacg.com/albums-index-page-7101.html
 
 // page number regex
-var patternNum = regexp.MustCompile(`(\d+)P`)
+var patternNum = regexp.MustCompile(`(\d+)P`) // 定义一个正则表达式，用于匹配页面数量
 
 // typical HonUrl /photos-index-aid-169728.html
 type GallaryInfo struct {
-	HonUrl string
-	Title  string
+	HonUrl string // 本子页面的URL
+	Title  string // 本子的标题
 }
 
 type HonDetail struct {
-	Tags    []string
-	Title   string
-	PageNum int      // 可以计算翻页次数
-	Images  []string // 按顺序的本子页面Url
+	Tags    []string // 本子的标签
+	Title   string   // 本子的标题
+	PageNum int      // 本子的页面数量，可以计算翻页次数
+	Images  []string // 按顺序的本子页面URL
 }
 
+// GenGallaryInfos 函数生成指定页面的相册信息
 func GenGallaryInfos(page int) (infos []*GallaryInfo) {
-	GallaryCollector := collector.Clone()
+	GallaryCollector := collector.Clone() // 克隆一个新的collector实例
 	GallaryCollector.OnHTML(".pic_box>a", func(e *colly.HTMLElement) {
+		fmt.Print("GenGallaryInfos 开始回调")
+		// 当匹配到.pic_box>a元素时，执行此回调函数
 		info := &GallaryInfo{}
-		info.HonUrl = e.Attr("href")
-		info.Title = e.Attr("title")
-		infos = append(infos, info)
+		info.HonUrl = e.Attr("href") // 获取href属性，赋值给HonUrl
+		info.Title = e.Attr("title") // 获取title属性，赋值给Title
+		infos = append(infos, info)  // 将info添加到infos切片中
 	})
 
 	GallaryCollector.OnRequest(func(r *colly.Request) {
-		fmt.Println("Requesting:", r.URL)
+		// 当发起请求时，执行此回调函数
+		fmt.Println("Requesting:", r.URL) // 打印请求的URL
 	})
 
-	url := fmt.Sprintf(GallaryUrl, page)
-	GallaryCollector.Visit(url)
+	// 设置 OnResponse 回调函数
+	GallaryCollector.OnResponse(func(r *colly.Response) {
+		fmt.Println("Received response from:", r.Request.URL)
+	})
+	// 设置 OnError 回调函数
+	GallaryCollector.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error: %s, %v\n", r.Request.URL, err)
+	})
+
+	url := fmt.Sprintf(GallaryUrl, page) // 格式化URL，替换占位符为page
+	GallaryCollector.Visit(url)          // 访问生成的URL
 	return
 }
 
+// GenHonDetails 函数生成指定相册信息的详细信息
 func GenHonDetails(g *GallaryInfo) (details *HonDetail) {
 	details = &HonDetail{
-		Tags:   make([]string, 0),
-		Title:  g.Title,
-		Images: make([]string, 0),
+		Tags:   make([]string, 0), // 初始化Tags切片
+		Title:  g.Title,           // 设置Title为相册信息的Title
+		Images: make([]string, 0), // 初始化Images切片
 	}
 
-	details.crawlTagAndPage(g)
-	details.crawlImages(g)
+	details.crawlTagAndPage(g) // 爬取标签和页面数量
+	details.crawlImages(g)     // 爬取图片URL
 
 	return
 }
 
+// crawlTagAndPage 方法爬取标签和页面数量
 func (hd *HonDetail) crawlTagAndPage(g *GallaryInfo) {
-	HonCollector := collector.Clone()
+	HonCollector := collector.Clone() // 克隆一个新的collector实例
 	// 对于标题信息的处理，获取Tags和PageNum
 	HonCollector.OnHTML(".uwconn", func(e *colly.HTMLElement) {
 		e.ForEach(".uwconn>label", func(i int, h *colly.HTMLElement) {
+			// 遍历.uwconn>label元素
 			if i == 0 { // 分类，解析到tag里，从0开始
-				tags := strings.Split(h.Text, " / ")
-				hd.Tags = append(hd.Tags, tags...)
+				tags := strings.Split(h.Text, " / ") // 按" / "分割标签字符串
+				hd.Tags = append(hd.Tags, tags...)   // 将标签添加到Tags切片中
 			}
 			if i == 1 { // 页数，解析到PageNum
-				pageStr := patternNum.FindAllStringSubmatch(h.Text, -1)
-				cnt, err := strconv.Atoi(pageStr[0][1])
+				pageStr := patternNum.FindAllStringSubmatch(h.Text, -1) // 使用正则表达式匹配页面数量
+				cnt, err := strconv.Atoi(pageStr[0][1])                 // 将匹配到的字符串转换为整数
 				if err != nil {
-					fmt.Printf("wrong with str unmarshal %v", pageStr)
+					fmt.Printf("wrong with str unmarshal %v", pageStr) // 打印错误信息
 				}
-				hd.PageNum = cnt
+				hd.PageNum = cnt // 设置PageNum为匹配到的页面数量
 			}
 		})
 		e.ForEach(".tagshow", func(_ int, h *colly.HTMLElement) {
-			hd.Tags = append(hd.Tags, h.Text)
+			// 遍历.tagshow元素
+			hd.Tags = append(hd.Tags, h.Text) // 将标签文本添加到Tags切片中
 		})
 	})
-	HonCollector.Visit(Host + g.HonUrl)
+	HonCollector.Visit(Host + g.HonUrl) // 访问本子页面的URL
 }
 
+// crawlImages 方法爬取图片URL
 func (hd *HonDetail) crawlImages(g *GallaryInfo) {
-	HonCollector := collector.Clone()
-	total := hd.PageNum/ImgsPerPage + 1
+	HonCollector := collector.Clone()   // 克隆一个新的collector实例
+	total := hd.PageNum/ImgsPerPage + 1 // 计算总页数
 	HonCollector.OnHTML(".pic_box>a", func(e *colly.HTMLElement) {
-		hd.Images = append(hd.Images, e.Attr("href"))
+		// 当匹配到.pic_box>a元素时，执行此回调函数
+		hd.Images = append(hd.Images, e.Attr("href")) // 将图片URL添加到Images切片中
 	})
 
-	for i := 1; i <= total; i++ {
-		url := pageUrlTrans(g.HonUrl, i)
-		HonCollector.Visit(Host + url)
+	for i := 1; i <= total; i++ { // 遍历所有页码
+		url := pageUrlTrans(g.HonUrl, i) // 生成每一页的URL
+		HonCollector.Visit(Host + url)   // 访问生成的URL
 	}
 }
